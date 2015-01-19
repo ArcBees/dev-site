8e3831f952f5fc4ca8fb493d530c58e947a1b2ec
larry <larry.matte@arcbees.com>
2015-01-19 14:23:44 -0500

    modif after a chat with manon & sp's comment

--- site/src/main/site/assets/js/global.js
+++ site/src/main/site/assets/js/global.js
@@ -11,16 +11,20 @@ $(function () {
         });
 
     // Arrow back to top in the footer
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('#backToTop').fadeIn('slow');
-        } else {
-            $('#backToTop').fadeOut('slow');
-        }
-    });
-    $('#backToTop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 'slow');
-        return false;
+    $("#backToTop").hide();
+
+    $(function () {
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 200) {
+                $('#backToTop').fadeIn('slow');
+            } else {
+                $('#backToTop').fadeOut('slow');
+            }
+        });
+        $('#backToTop').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 'slow');
+                return false;
+            });
     });
 
     // FlexNav
