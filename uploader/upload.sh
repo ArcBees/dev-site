93e55db387074d02dea02cf62066132e4a45a950
Christopher Viel <viel.christopher@gmail.com>
2015-04-23 16:15:22 -0400

    Add a gwtp-staging profile

--- uploader/upload.sh
+++ uploader/upload.sh
@@ -4,4 +4,4 @@
 # Run with 'credentials' (after running save_credentials.sh) to deploy to prod.
 
 mvn -q compile generate-resources
-mvn -q exec:java -Dexec.mainClass=com.google.gwt.site.uploader.Uploader -Dexec.args="target/gwt-site-unpack $1"
+mvn -q exec:java -Dexec.mainClass=com.google.gwt.site.uploader.Uploader -Dexec.args="target/arcbees-markdown-unpack $1"
